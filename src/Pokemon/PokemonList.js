1b192ee6c4e73b85af3f5e5000481d7037d1f6f7
kayla schoorl <kaylaschoorl@gmail.com>
2021-04-28 17:05:32 -0700

    added folders

---
+++ src/Pokemon/PokemonList.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+export default class PokemonList extends Component {
+
+
+
+
+
+
+  <ul>
+    <PokemonItem/>
+    <PokemonItem/>
+    <PokemonItem/>
+    <PokemonItem/>
+    <PokemonItem/>
+    <PokemonItem/>
+  </ul>
+}

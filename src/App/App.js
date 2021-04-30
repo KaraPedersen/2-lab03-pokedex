c377d20513a77f61c2ae22397999387ee34de1d7
kayla schoorl <kaylaschoorl@gmail.com>
2021-04-30 13:03:48 -0700

    Updated app.js

--- src/App/App.js
+++ src/App/App.js
@@ -4,26 +4,97 @@ import Footer from './Footer';
 import './App.scss';
 import PokemonList from '../Pokemon/PokemonList';
 import request from 'superagent';
+import Search from './Search';
+import Paging from './Paging';
+
+const POKEMON_API_URL = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
 
 class App extends Component {
 state={
-  pokemon: []
+  pokemon: null,
+  loading: false,
+  search: '',
+  page: 1
 }
-componentDidMount = async () => {
-  const response = await request.get('https://pokedex-alchemy.herokuapp.com/api/pokedex');
-  this.setState({ pokemon: response.body.results });
+
+componentDidMount() {
+  this.fetchPokemon();
 }
+
+async fetchPokemon() {
+  const { search, page } = this.state;
+
+  this.setState({ loading: true });
+
+  try {
+    const response = await request
+      .get(POKEMON_API_URL)
+      .query({ name: search })
+      .query({ page: page });
+
+    this.setState({ pokemon: response.body });
+  }
+  catch (err) {
+    console.log(err);
+  }
+  finally {
+    this.setState({ loading: false });
+  }
+}
+handleSearch = ({ search }) => {
+  this.setState(
+    { search: search, page: 1 },
+    () => this.fetchPokemon()
+  );
+}
+
+handlePrevPage = () => {
+  this.setState(
+    { page: Math.max(this.state.page - 1, 1) },
+    () => this.fetchPokemon() 
+  );
+  
+}
+
+handleNextPage = () => {
+  this.setState(
+    { page: this.state.page + 1 },
+    () => this.fetchPokemon()
+  );
+}
+
 render() {
-  const { pokemon } = this.state;
+  const { pokemon, loading, page } = this.state;
   return (
     <div className="App">
+
       <Header/>
-      <PokemonList pokemon={pokemon}/>
+      
+      <section className="search-options">
+        <Search onSearch={this.handleSearch}/>
+        <Paging 
+          page={page} 
+          onPrev={this.handlePrevPage} 
+          onNext={this.handleNextPage}
+        />
+      </section>
+      
+      <main>
+        {pokemon && (pokemon.length
+          ? <PokemonList pokemon={pokemon}/>
+          : <p>Sorry no pokemons :(</p>)
+        }
+
+        {loading && <img className="loading" src="loading.gif" alt="loading"/>}
+
+      </main>
+    
       <Footer/>
-        
+
     </div>
   );
 }
+
 }
 
 export default App;

7648a2fb9ea77f3fd1791b1348a41343bb5497b2
kayla schoorl <kaylaschoorl@gmail.com>
2021-04-30 14:49:19 -0700

    added photo for loading

--- src/common/Loading.js
+++ src/common/Loading.js
@@ -9,7 +9,7 @@ export default class Loading extends Component {
     if (!loading) return null;
 
     return (
-      <img className="Loading" src="loading.gif" alt="loading"/>
+      <img className="Loading" src='../assets/you-suck.jpeg' alt="loading"/>
     );
   }
 }
